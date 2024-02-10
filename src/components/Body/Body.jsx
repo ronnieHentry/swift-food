2a29c094b20cefc2ebbf194ca3f1e47a2b8f1447
unknown <ronniehentry@gmail.com>
2024-02-11 00:03:40 +0530

    Made restaurant data dynamic - Added API call

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -1,15 +1,42 @@
+import { useState, useEffect } from "react";
 import RestaurantCard from "../RestaurantCard/RestaurantCard";
 import "./Body.css";
-import { restaurantData } from "../../../public/RestaurantData";
 
 const Body = () => {
+  const [restaurantList, setRestaurantList] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=8.595232345933871&lng=76.8937373533845&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await data.json();
+    const filteredRestaurantList = json?.data?.cards?.filter(
+      (item) => item?.card?.card?.id === "top_brands_for_you"
+    );
+    setRestaurantList(
+      filteredRestaurantList[0]?.card?.card?.gridElements?.infoWithStyle
+        ?.restaurants
+    );
+  };
+
   return (
     <div className="body">
       <div className="search">Search</div>
       <div className="restro-container">
-        {restaurantData.map((restaurant, index) => {
-          return <RestaurantCard data={restaurant.info} key={restaurant.info.id} />;
-        })}
+        {restaurantList.length
+          ? restaurantList.map((restaurant, index) => {
+              return (
+                <RestaurantCard
+                  data={restaurant.info}
+                  key={restaurant.info.id}
+                />
+              );
+            })
+          : null}
       </div>
     </div>
   );
